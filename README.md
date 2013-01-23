5d22d621a9c0afca40c5acc84e8b2f3acc55f36d
Peilin Yang <yangpeilyn@gmail.com>
2013-01-23 16:34:14 -0500

    First Version
    This is the first version

--- README.md
+++
@@ -1,4 +0,0 @@
-fetch_sms_and_browser_hist
-==========================
-
-Fetch SMS and Browser History on Android. Target SDK Version is 17.
---
+++ gen/com/example/buttontest/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.buttontest;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
